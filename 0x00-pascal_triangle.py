93bdb6762ed05dbb66b5a6747fc60a614585d785
firebreather-heart <dtenny95@gmail.com>
2023-12-01 19:28:56 +0100

    interview

---
+++ 0x00-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n:int):
+    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    if n <=0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+if __name__ == "__main__":
+    print(pascal_triangle(5))
